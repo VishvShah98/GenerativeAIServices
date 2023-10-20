84896171341413c4a2a9753ae024149dce9b7bc8
vishv shah <vishvshahwork@gmail.com>
2023-10-20 09:59:12 -0400

    changed testimonials height

--- src/pages/Testimonials.jsx
+++ src/pages/Testimonials.jsx
@@ -120,7 +120,7 @@ function Testimonials() {
           </button>
         </div>
 
-        <div className="flex-grow relative text-center flex justify-center items-center w-full h-[250px] md:h-[400px] h-[500px] border border-red-500 ">
+        <div className="flex-grow relative text-center flex justify-center items-center w-full h-[250px] md:h-[400px] h-[500px]">
           <AnimatePresence
             initial={false}
             custom={direction}
